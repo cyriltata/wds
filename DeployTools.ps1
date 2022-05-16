55b60744bfd4fc5a0f71d22bc0bca0d34ceef629
Cyril <cyril.tata@gmail.com>
2022-05-16 10:59:35 +0200

    Update DeployTools.ps1

--- DeployTools.ps1
+++ DeployTools.ps1
@@ -5,12 +5,12 @@ cd $toolsdir
 $filename = "Inquisit_50140.exe"
 Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
 Start-Process -Wait -FilePath $filename -PassThru
-#del $filename
+# del $filename
 
 $filename = "PowerToysSetup-0.58.0-x64.exe"
 Invoke-WebRequest -uri "https://github.com/microsoft/PowerToys/releases/download/v0.58.0/PowerToysSetup-0.58.0-x64.exe" -outFile $filename
 Start-Process -Wait -FilePath $filename -ArgumentList "/S" -PassThru
-del $filename
+# del $filename
 
-#cd ../
-#rmdir $toolsdir
+# cd ../
+# rmdir $toolsdir
