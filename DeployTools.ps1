6e4a3e70945774115f82218c0ac43a509e313ae3
Cyril <cyril.tata@gmail.com>
2022-05-16 10:23:42 +0200

    Update DeployTools.ps1

--- DeployTools.ps1
+++ DeployTools.ps1
@@ -4,7 +4,7 @@ cd $toolsdir
 
 $filename = "Inquisit_50140.exe"
 Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
-Start-Process -Wait msiexec.exe -ArgumentList "/i $filename /qn"
+Start-Process -Wait -FilePath $filename -ArgumentList "/S" -PassThru
 del $filename
-cd C:\
+cd ../
 rmdir $toolsdir
