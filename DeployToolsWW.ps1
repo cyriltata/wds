5cb66506ee4d199fcf1a55070054fd4d39870275
Cyril <cyril.tata@gmail.com>
2022-05-16 15:23:36 +0200

    Update and rename DeployTools.ps1 to DeployToolsWW.ps1
    add commands to execute after deploy

--- DeployToolsWW.ps1
+++ DeployToolsWW.ps1
@@ -4,13 +4,17 @@ cd $toolsdir
 
 $filename = "Inquisit_50140.exe"
 Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
-Start-Process -Wait -FilePath $filename -PassThru
+Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
 # del $filename
 
 $filename = "PowerToysSetup-0.58.0-x64.exe"
 Invoke-WebRequest -uri "https://github.com/microsoft/PowerToys/releases/download/v0.58.0/PowerToysSetup-0.58.0-x64.exe" -outFile $filename
-Start-Process -Wait -FilePath $filename -ArgumentList "/S" -PassThru
+Start-Process -Wait -FilePath $filename -ArgumentList "/install /passive /log PowerToys.log" -PassThru
 # del $filename
 
 # cd ../
 # rmdir $toolsdir
+
+Disable-NetAdapter -Name '*' -Confirm:$false"
+
+Get-PnpDevice -class 'MEDIA' | ForEach-Object {Disable-PnpDevice -InstanceId $_.InstanceID -Confirm:$false}"
