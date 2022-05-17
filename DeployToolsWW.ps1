36e0131eccfd1cf4fa5569562278157189134a82
Cyril <cyril.tata@gmail.com>
2022-05-17 21:48:48 +0200

    Update DeployToolsWW.ps1

--- DeployToolsWW.ps1
+++ DeployToolsWW.ps1
@@ -1,22 +1,22 @@
 $toolsdir = "C:\WDSTools"
-mkdir $toolsdir
-cd $toolsdir
+#mkdir $toolsdir
+#cd $toolsdir
 
-$filename = "Inquisit_50140.exe"
-Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
-Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
-del $filename
+#$filename = "Inquisit_50140.exe"
+#Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
+#Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
+#del $filename
 
-$filename = "remap-keys.reg"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/cyriltata/wds/main/remap-keys.reg" -outFile $filename
-reg import $filename
-del $filename
+#$filename = "remap-keys.reg"
+#Invoke-WebRequest -uri "https://raw.githubusercontent.com/cyriltata/wds/main/remap-keys.reg" -outFile $filename
+#reg import $filename
+#del $filename
 
 #cd ../
 #rmdir $toolsdir
 
-Disable-NetAdapter -Name 'WLAN' -Confirm:$false
+#Disable-NetAdapter -Name 'WLAN' -Confirm:$false
 
-Get-PnpDevice -class 'MEDIA' | ForEach-Object {Disable-PnpDevice -InstanceId $_.InstanceID -Confirm:$false}
+#Get-PnpDevice -class 'MEDIA' | ForEach-Object {Disable-PnpDevice -InstanceId $_.InstanceID -Confirm:$false}
 
 
