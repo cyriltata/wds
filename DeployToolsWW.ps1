77f11b2bceaeb72567aac3e7e13cd0bbc6d0e028
Cyril <cyril.tata@gmail.com>
2022-05-17 16:11:06 +0200

    Disable commands

--- DeployToolsWW.ps1
+++ DeployToolsWW.ps1
@@ -1,15 +1,15 @@
 $toolsdir = "C:\WDSTools"
-mkdir $toolsdir
-cd $toolsdir
+#mkdir $toolsdir
+#cd $toolsdir
 
-$filename = "PowerToysSetup-0.58.0-x64.exe"
-Invoke-WebRequest -uri "https://github.com/microsoft/PowerToys/releases/download/v0.58.0/PowerToysSetup-0.58.0-x64.exe" -outFile $filename
-Start-Process -Wait -FilePath $filename -ArgumentList "/install /passive /log PowerToys.log" -PassThru
-# del $filename
+#$filename = "PowerToysSetup-0.58.0-x64.exe"
+#Invoke-WebRequest -uri "https://github.com/microsoft/PowerToys/releases/download/v0.58.0/PowerToysSetup-0.58.0-x64.exe" -outFile $filename
+#Start-Process -Wait -FilePath $filename -ArgumentList "/install /passive /log PowerToys.log" -PassThru
+#del $filename
 
-$filename = "Inquisit_50140.exe"
-Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
-Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
+#$filename = "Inquisit_50140.exe"
+#Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
+#Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
 # del $filename
 
 # cd ../
