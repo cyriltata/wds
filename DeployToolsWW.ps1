7f65464db4a31b974e174921223e79678bf3db64
Cyril <cyril.tata@gmail.com>
2022-05-17 20:42:19 +0200

    Update DeployToolsWW.ps1

--- DeployToolsWW.ps1
+++ DeployToolsWW.ps1
@@ -2,11 +2,14 @@ $toolsdir = "C:\WDSTools"
 mkdir $toolsdir
 cd $toolsdir
 
-# @TODO download key registry and reg import
-
 $filename = "Inquisit_50140.exe"
 Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
 Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
+del $filename
+
+$filename = "remap-keys.reg"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/cyriltata/wds/main/remap-keys.reg" -outFile $filename
+reg import $filename
 del $filename
 
 cd ../
