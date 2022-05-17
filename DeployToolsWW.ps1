86e50dbdd7632ef08373918bd6aedcef53e771e7
Cyril <cyril.tata@gmail.com>
2022-05-17 20:17:17 +0200

    Update DeployToolsWW.ps1

--- DeployToolsWW.ps1
+++ DeployToolsWW.ps1
@@ -1,20 +1,22 @@
 $toolsdir = "C:\WDSTools"
-#mkdir $toolsdir
-#cd $toolsdir
+mkdir $toolsdir
+cd $toolsdir
 
 #$filename = "PowerToysSetup-0.58.0-x64.exe"
 #Invoke-WebRequest -uri "https://github.com/microsoft/PowerToys/releases/download/v0.58.0/PowerToysSetup-0.58.0-x64.exe" -outFile $filename
 #Start-Process -Wait -FilePath $filename -ArgumentList "/install /passive /log PowerToys.log" -PassThru
 #del $filename
 
-#$filename = "Inquisit_50140.exe"
-#Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
-#Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
-# del $filename
+$filename = "Inquisit_50140.exe"
+Invoke-WebRequest -uri "https://inquisit.millisecond.com/5/5_0_14_0/win/Inquisit_50140.exe" -outFile $filename
+Start-Process -Wait -FilePath $filename -ArgumentList "/Q" -PassThru
+del $filename
 
-# cd ../
-# rmdir $toolsdir
+cd ../
+rmdir $toolsdir
 
-#Disable-NetAdapter -Name '*' -Confirm:$false
+Disable-NetAdapter -Name 'WLAN' -Confirm:$false
 
-#Get-PnpDevice -class 'MEDIA' | ForEach-Object {Disable-PnpDevice -InstanceId $_.InstanceID -Confirm:$false}
+Get-PnpDevice -class 'MEDIA' | ForEach-Object {Disable-PnpDevice -InstanceId $_.InstanceID -Confirm:$false}
+
+# @TODO download key registry and reg import
