49e9ea60db2ebf38e4cedf8f8172015c3c826214
Cyril <cyril.tata@gmail.com>
2022-05-18 11:13:46 +0200

    Test if path exists before creating it

--- FirstLogon.ps1
+++ FirstLogon.ps1
@@ -1,11 +1,20 @@
-# Disable notification center
-New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -force
+# ## Disable notification center ##
+ 
+if (-Not (Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
+    New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -force
+}
 Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type "DWORD" -Value 1
 
-# Disable context menu
-New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Explorer" -force
+# ## Disable context menu ##
+if (-Not (Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Explorer" -force
+}
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoViewContextMenu" -Type "DWORD" -Value 1
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoTrayContextMenu" -Type "DWORD" -Value 1
 
-New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Explorer" -force
+if (-Not (Test-Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
+    New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Explorer" -force
+}
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoTrayContextMenu" -Type "DWORD" -Value 1
+
+## @TODO: Disable others
